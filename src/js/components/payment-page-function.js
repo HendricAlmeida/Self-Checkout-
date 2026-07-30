ed4413b144d5f59aea3ae4cdc6bc19d56ce0efa6
Hendric Almeida <hendricalmeida304@gmail.com>
2026-07-30 11:04:00 -0300

    Atualizações do funcionamento do DOM

--- src/js/components/payment-page-function.js
+++ src/js/components/payment-page-function.js
@@ -1,5 +1,9 @@
 const containerPay = document.querySelector(".payment-section");
+const priceValueContent = document.querySelector('#price-value')
+const containerMain = document.querySelector('.container-main')
 let proceedActive = null
+
+//efeito do butão
 function btnActive(escolhido) {
     const btn = document.querySelectorAll(".btn");
     //remove the posibilite they have the "active"
@@ -14,8 +18,12 @@ function btnActive(escolhido) {
 
 function removeDisplayPay(){
     containerPay.style.display = "none"
+    containerMain.classList.remove("disable")
+    document.querySelector(".search-input").disabled = false;
 }
 
 function addDisplayPay() {
+    priceValueContent.innerHTML = `R$${priceValueOver.toFixed(2)}`
+    document.querySelector(".search-input").disabled = true;
     containerPay.style.display = "flex"
 }
