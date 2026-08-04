0e1a025858cb12008c743ad959d7d4ea055eb7b3
Hendric Almeida <hendricalmeida304@gmail.com>
2026-08-04 15:33:05 -0300

    Adição de Funcionalidades na Barra de Pesquisa, Adição de acessibilidade + Aperfecionamento

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -2,6 +2,35 @@ const inputSearch = document.querySelector('.search-input');
 const itemBox = document.querySelector('.container-itens-seached');
 const itemOverCont = document.querySelector('#item-overall')
 
+inputSearch.addEventListener('input', (event) => {
+    const value = formString(event.target.value);
+    const items = document.querySelectorAll('.container-itens-seached .item-seached');
+    const noResult = document.querySelector('.no-result');
+    let hasResult = false
+
+    items.forEach(item => {
+        if(formString(item.textContent).indexOf(value) !== -1){
+            item.style.display = 'flex';
+            hasResult = true
+        }else{
+            item.style.display = 'none'
+            console.log('cheguei')
+        }
+    })
+    
+    if(hasResult){
+        noResult.style.display = 'none';
+    }else{
+        noResult.style.display = 'block';
+    }
+});
+
+function formString(value){
+    return value
+    .toLowerCase()
+    .trim()
+    .normalize('NFD')
+}
 
 function onSearch() {
     itemBox.style.display = "flex"
